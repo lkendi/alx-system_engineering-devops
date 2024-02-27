99618811ff140e5a2f8d3b32ff827c4f63e2c32b
lkendi <lkendi003@gmail.com>
2024-02-27 21:15:15 +0300

    Edit task 0 script

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/env ruby
 regex = /School/
-matches = ""
-
-for arg in ARGV
-  if arg =~ regex
-    matches+=arg
-  else
-    next
-  end
-end
-puts matches
+puts ARGV[0].scan(regex).join
 
