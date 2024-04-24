10c323946ff3f91f25588a0f6b4088e5c5a8ab09
lkendi <lkendi003@gmail.com>
2024-04-24 06:43:43 +0300

    Add task 1 puppet manifest

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+# Using Puppet install flask (version 2.1.0) from pip3
+exec { 'install_flask':
+  command     => 'pip3 install flask==2.1.0',
+  path        => 'usr/bin/',
+  refreshonly => true,
+  unless      => 'pip3 show flask | grep -q "Version: 2.1.0"'
+}
