0ebf32c09cfe7091e7ed9528edc79daff4b8a50e
lkendi <lkendi003@gmail.com>
2024-04-24 06:51:06 +0300

    Add task 1 and 2 files

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Using Puppet install flask (version 2.1.0) from pip3
 exec { 'install_flask':
-  command     => 'pip3 install flask==2.1.0',
-  path        => 'usr/bin/',
-  refreshonly => true,
-  unless      => 'pip3 show flask | grep -q "Version: 2.1.0"'
+  command => 'pip3 install flask==2.1.0',
+  path    => 'usr/bin/',
+  unles   => 'pip3 show flask | grep -q "Version: 2.1.0"'
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Create a manifest that kills a process named 'killmenow'
+# Must use exec Puppet resource and pkill
+
+exec {'kill_process':
+  command => 'pkill killmenow',
+  path    => 'usr/bin/'
+}
