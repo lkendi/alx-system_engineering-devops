45e3a11017f14f6f5749cc3c79208d114803a9fd
lkendi <lkendi003@gmail.com>
2024-07-11 10:55:11 +0300

    Fix puppet-lint errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Fix Apache 500 error
 
 exec { 'fix':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path     => ['/bin', '/usr/bin'],
-  onlyif   => 'grep -q "phpp" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'grep -q "phpp" /var/www/html/wp-settings.php',
 }
